a38017565d1b205886d092ba3c50c17eaa500023
Hanna Selim <hannaselim@Hannas-MacBook-Pro.local>
2019-02-06 15:40:18 +0100

    idk

--- java.js
+++ java.js
@@ -1,9 +1,1 @@
-const template = document.querySelector("#myTemp").content;
-const main = document.querySelector("main");
-for(let i = 0; i<10; i++){
-let clone = template.cloneNode(true);
-    clone.querySelector("h1").textContent="Name of meal";
-    clone.querySelector("h2").textContent="59,- kr.";
-    clone.querySelector("img").src="guzni-md.jpg";
-    main.appendChild(clone);
-}
+
